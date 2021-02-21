0e3a0798b68ad5dbe77f7a5e1ecd65a9c1ab65d8
EC2 Default User <ec2-user@ip-172-31-13-103.us-east-2.compute.internal>
2021-02-21 23:39:01 +0000

    HW3

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,38 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
+
+Then /(.*) seed movies should exist/ do | n_seeds |
+  Movie.count.should be n_seeds.to_i
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  expect(page.body.index(e1)).to be < page.body.index(e2)
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(', ').each do |rating|
+    if uncheck
+      uncheck("ratings[#{rating}]")
+    else
+      check("ratings[#{rating}]")
+    end
+  end
+end
+
+Then /I should see all the movies/ do
+  expect(page).to have_selector('tbody tr', count: Movie.count)
+end
